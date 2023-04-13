12f1c09a864107885df2efa052ecb83fbf088ed2
Alberto Caravantes Arranz <acaravantesnov@hotmail.com>
2023-04-13 19:54:34 -0400

    xor and andi xori srli

--- RISCVImplementation.srcs/sources_1/new/InstructionMemory.vhd
+++ RISCVImplementation.srcs/sources_1/new/InstructionMemory.vhd
@@ -21,7 +21,7 @@ use ieee.std_logic_textio.all;
 entity InstructionMemory is
   generic(
     INS_MEM_SIZE: integer := 2 ** 8;
-    TEXT_FILE:		string := "slti.mem"
+    TEXT_FILE:		string := "srli.mem"
   );
   port(
     readAddress: in std_logic_vector(31 downto 0);
